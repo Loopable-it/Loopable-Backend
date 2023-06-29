cd136954418a4028e8635ffd65c02bf89cc1a0c3
vellons <vellons@users.noreply.github.com>
2023-06-29 23:02:51 +0200

    Created models Products, ProductImage and Category

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
@@ -18,6 +20,9 @@ class Profile(models.Model):
     type = models.CharField(max_length=3, choices=PROFILE_TYPE_CHOICES, default='STD')
     is_verified = models.BooleanField(default=False)
     sign_in_provider = models.CharField(max_length=32)
+    province = models.CharField(max_length=64, null=True)
+    is_complete = models.BooleanField(default=False)
+    image = models.CharField(max_length=1024, null=True)
     fcm_token = models.CharField(max_length=256, null=True)  # Firebase Cloud Messaging Token
     allow_notifications = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -34,3 +39,44 @@ class Profile(models.Model):
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(id=instance.username, user=instance)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=32, primary_key=True, unique=True)
+    description = models.CharField(max_length=512, null=True)
+
+    def __repr__(self):
+        return '<Category {}>'.format(self.name)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=2048)
+    owner = models.ForeignKey(Profile, on_delete=models.PROTECT, related_name='products')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products_list')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return '<Product {} {}>'.format(self.id, self.name)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+class ProductImage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.CharField(max_length=1024)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __repr__(self):
+        return '<ProductImage {} {}>'.format(self.id, self.image)
+
+    def __str__(self):
+        return '{}'.format(self.image)
