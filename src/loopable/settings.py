668cd81278cfc2875304c6f49d5b06b34602ba2c
vellons <vellons@users.noreply.github.com>
2023-11-28 22:45:35 +0100

    fix: force serializer to return float instead of string for decimal

--- src/loopable/settings.py
+++ src/loopable/settings.py
@@ -176,6 +176,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+    'COERCE_DECIMAL_TO_STRING': False,
 }
 
 # Swagger
